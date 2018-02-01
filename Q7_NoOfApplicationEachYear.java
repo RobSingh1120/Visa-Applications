cb4958333b82171fda84bea6999da06d34a35aaa
robinsinghpoonia <33062223+robinsinghpoonia@users.noreply.github.com>
2018-02-01 23:08:29 +0530

    Add files via upload

---
+++ Q7_NoOfApplicationEachYear.java
@@ -0,0 +1,100 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class Q7_NoOfApplicationEachYear 
+{
+  
+	
+	//Mapper Class
+	public static class MapperClass extends Mapper <LongWritable, Text, Text, IntWritable>
+	{
+
+		@Override
+		protected void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException 
+		{
+			String mySearchText = context.getConfiguration().get("myText");
+			
+			String[] str = value.toString().split("\t");
+			String year = str[7];
+			
+			if(mySearchText.equals("ALL"))
+			{
+			context.write(new Text(year), new IntWritable(1));
+			}
+			else
+			{
+				  if(year.equals(mySearchText))
+				
+				{
+					  context.write(new Text(year), new IntWritable(1));
+				}
+			}
+		
+		}
+		
+	}
+	
+	
+	//Reducer Class
+	public static class ReduceClass extends Reducer <Text, IntWritable, Text, IntWritable>
+	{
+
+		@Override
+		protected void reduce(Text key, Iterable<IntWritable> values,Context context)throws IOException, InterruptedException 
+		{
+			int sum=0;
+			for(IntWritable value : values)
+			{
+				sum += value.get();
+			}
+			
+			context.write(new Text(key), new IntWritable(sum));
+		}
+		
+	}
+	
+	
+	  //Main Method
+	public static void main(String[] args ) throws Exception
+	{
+		Configuration conf = new Configuration();
+		
+		  if(args.length > 2)
+		  {
+			  
+			   conf.set("myText", args[2]);  
+		  }
+		  
+		Job job = Job.getInstance(conf, "The Number of Applications for each year");
+	    job.setJarByClass(Q7_NoOfApplicationEachYear.class);
+	    
+	    job.setMapperClass(MapperClass.class);
+	    job.setReducerClass(ReduceClass.class);
+	    
+	    job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(IntWritable.class);
+	    
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+	    
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	    
+	    
+	    }
+	
+	
+}
